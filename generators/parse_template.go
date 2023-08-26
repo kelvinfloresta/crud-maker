fe03c7dea983c0fc5b3979bfc4f379418ad5da0b
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2023-08-26 01:49:51 -0300

    fix(ParseTemplate): add missing fields_optional

--- generators/parse_template.go
+++ generators/parse_template.go
@@ -19,20 +19,23 @@ type ParseTemplateInput struct {
 
 func ParseTemplate(input ParseTemplateInput) string {
 	var (
-		fields      = ""
-		fieldsModel = ""
-		adaptInput  = ""
+		fields         = ""
+		fieldsModel    = ""
+		adaptInput     = ""
+		fieldsOptional = ""
 	)
 
 	for fieldName, field := range input.Fields {
 		fields += parseField(fieldName, field)
 		fieldsModel += parseModel(fieldName, field)
 		adaptInput += fmt.Sprintf("%s: input.%s,\n", fieldName, fieldName)
+		fieldsOptional += fmt.Sprintf("%s *%s\n", fieldName, field.Type)
 	}
 
 	template := strings.ReplaceAll(input.Template, "{{adapt_input}}", adaptInput)
 	template = strings.ReplaceAll(template, "{{fields}}", fields)
 	template = strings.ReplaceAll(template, "{{fields_model}}", fieldsModel)
+	template = strings.ReplaceAll(template, "{{fields_optional}}", fieldsOptional)
 	template = strings.ReplaceAll(template, "{{method_input}}", input.MethodInput)
 	template = strings.ReplaceAll(template, "{{method_output}}", input.MethodOutput)
 	template = strings.ReplaceAll(template, "{{name}}", strings.ToLower(input.Name))
@@ -40,7 +43,6 @@ func ParseTemplate(input ParseTemplateInput) string {
 	template = strings.ReplaceAll(template, "{{method_capitalized}}", strings.Title(input.MethodName))
 	template = strings.ReplaceAll(template, "{{name_plural}}", utils.ToSnakeCase(input.NamePlural))
 	template = strings.ReplaceAll(template, "{{project_name}}", config.ProjectName)
-
 	return template
 }
 
