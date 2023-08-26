3752c545acb55991ead366fa1abcd37055f01aec
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2023-08-26 01:18:48 -0300

    refactor(GatewayAdapter): move create logic to own file

---
+++ generators/create/gateway_adapter.go
@@ -0,0 +1,40 @@
+package create
+
+import (
+	"crud-maker/generators"
+	"crud-maker/utils"
+	"fmt"
+	"strings"
+)
+
+type CreateGatewayAdapter struct {
+	name       string
+	namePlural string
+	outputFile string
+	fields     map[string]generators.Field
+}
+
+func NewGatewayAdapter(name, namePlural string, fields map[string]generators.Field) *CreateGatewayAdapter {
+	return &CreateGatewayAdapter{
+		name:       name,
+		namePlural: namePlural,
+		fields:     fields,
+		outputFile: fmt.Sprintf("adapters/gateways/%s_gateway/gorm_create.go", strings.ToLower(name)),
+	}
+}
+
+func (c *CreateGatewayAdapter) Generate() {
+	template := utils.ReadTemplate("gateway_gorm_create.template")
+
+	template = generators.ParseTemplate(generators.ParseTemplateInput{
+		Template:     template,
+		Name:         c.name,
+		NamePlural:   c.namePlural,
+		Fields:       c.fields,
+		MethodName:   "Create",
+		MethodInput:  "input CreateInput",
+		MethodOutput: "(string, error)",
+	})
+
+	utils.WriteTemplate(template, c.outputFile)
+}
